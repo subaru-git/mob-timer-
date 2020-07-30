b09b84fc644c62111e178121e4d9a1a2f8323868
subaru-git <subaru12081985@yahoo.co.jp>
2020-07-30 17:13:42 +0900

    add logo

--- src/containers/Top/TimerTop.tsx
+++ src/containers/Top/TimerTop.tsx
@@ -7,14 +7,24 @@ import { TimerAppBar } from 'components/common/TimerAppBar';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
+import Logo from '../../asset/mobtimer-logo.svg';
+
 const useStyles = makeStyles(() =>
   createStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'nowrap',
+    },
+    logo: {
+      marginRight: '20px',
+    },
     sharedLink: {
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'center',
+      flexWrap: 'wrap',
       alignItems: 'center',
-      margin: '0 auto',
       height: '100vh',
     },
   }),
@@ -29,23 +39,28 @@ const TimerTop: FC = () => {
     <>
       <TimerAppBar menu={false} />
       <div className={classes.sharedLink}>
-        <Typography variant="h4">http://i-do-not-get-domain.sorry/</Typography>
-        <TextField
-          value={name}
-          label="create shared link"
-          helperText="your original room name"
-          variant="filled"
-          onChange={(e: any) => {
-            setName(e.target.value);
-          }}
-        />
-        <IconButton
-          onClick={() => {
-            if (name !== '') history.push(`/${name}`);
-          }}
-        >
-          <ArrowForwardIcon />
-        </IconButton>
+        <img className={classes.logo} src={Logo} alt="logo" />
+        <div className={classes.sharedLink}>
+          <Typography variant="h4">
+            http://i-do-not-get-domain.sorry/
+          </Typography>
+          <TextField
+            value={name}
+            label="create shared link"
+            helperText="your original room name"
+            variant="filled"
+            onChange={(e: any) => {
+              setName(e.target.value);
+            }}
+          />
+          <IconButton
+            onClick={() => {
+              if (name !== '') history.push(`/${name}`);
+            }}
+          >
+            <ArrowForwardIcon />
+          </IconButton>
+        </div>
       </div>
     </>
   );
